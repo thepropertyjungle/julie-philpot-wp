9607ee43b225c66be3cda658fc62ae6497300bab
mgrant739 <michael.grant@thepropertyjungle.com>
2025-02-24 15:29:05 +0000

    brochure amends on prop single

--- blade-views/single-property.blade.php
+++ blade-views/single-property.blade.php
@@ -298,7 +298,24 @@ Video</a>
 <!--epc -->  
 
 
+<!--Brochure -->  
+@if(!empty($property['brochures']))
+             
+             @if(is_array($property['brochures'] ?? false))
+             @foreach ($property['brochures'] as $property_brochure)    
+<a href="{{ $property_brochure['media_url'] ?? '' }}" class="property-details__summary-link scroll"  target="_blank"  >
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <path d="M2 3h6a2 2 0 0 1 2 2v16H4a2 2 0 0 1-2-2V3z"/>
+  <path d="M22 3h-6a2 2 0 0 0-2 2v16h6a2 2 0 0 0 2-2V3z"/>
+  <path d="M8 3v16"/>
+  <path d="M16 3v16"/>
+</svg>
 
+    Brochure</a>
+    @endforeach
+                    @endif                    
+                
+                @endif
 
 
 
